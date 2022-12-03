b5a51a8c8d89343e8536f49327e6de8a404835b6
tungstenmeteor <45355413+tungstenmeteor@users.noreply.github.com>
2022-12-02 18:20:59 -0800

    New Textbox

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,16 @@
 import React, { useState } from "react";
 import "./App.css";
-import styled from 'styled-components';
-import { SafeAreaView, StyleSheet, TextInput } from "react-native";
-import TextField from '@mui/material/Button';
+import ReactDOM from 'react-dom/client';
 
+const [cardList, setCardList] = useState([]) ;
 
-let mainArray = [] ;
-
-
-function App() {
-
-  return ( 
-<SafeAreaView>
-  
-  <TextField id="outlined-basic" label="Kingdom" variant="outlined" />
-
+function App(){
+return (
   <div>
-  Kingdom Cards:
-</div>
-<div>
-  {
-    [mainArray]
-  }
+  <div>
+            
+  Kingdom Cards:  
+  {cardList}
 </div>
 <div> 
   ---
@@ -29,18 +18,75 @@ function App() {
 <div>
   Landscape Cards:
 </div>
-</SafeAreaView>
-  );
+<div> 
+  ---
+</div>
+<div>
+  Plays With:
+</div>
+</div>
+)
 
 }
 
-const styles = StyleSheet.create({
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-  },
-});
+export default class Input extends React.Component {
+  render() {
 
-export default App;
+    class NameForm extends React.Component {
+      constructor(props) {
+        super(props);
+        this.state = {value: ''};
+    
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+    
+      handleChange(event) {
+        this.setState({value: event.target.value});
+        this.setCardList({value: event.target.value});
+        console.log (setCardList)
+      }
+    
+      handleSubmit(event) {
+        event.preventDefault();
+      }
+    
+      render() {
+        return (
+          <div>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Card List:
+              <input type="text" value={this.state.value} onChange={this.handleChange} />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+          <div>---</div>
+            <div>
+            </div>
+          </div>
+        );
+      }
+    }
+    
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(<NameForm />);
+
+  return ( 
+  <div className = "App">
+<div>
+  
+  Kingdom Cards:  
+  
+</div>
+<div> 
+  ---
+</div>
+<div>
+  Landscape Cards:
+</div>
+</div>
+  );
+
+}
+}
---
+++ frontend/src/MuiTextField.js
@@ -0,0 +1,9 @@
+import { Stack } from "@mui/material";
+
+export const MuiTextField = () => {
+    <Stack spacing ={4}>
+    <Stack direction='row' spacing={2}>
+      <TextField label='Kingdom' />
+    </Stack>
+  </Stack>
+}
